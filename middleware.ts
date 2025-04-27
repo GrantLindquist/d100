7322c1a8b83774d19737297bd9f8dd89375d107d
grant-lindquist <grant-lindquist@outlook.com>
2025-04-26 17:40:24 -0700

    Updated middleware to prevent sign-in screen from being shown prematurely

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,24 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { getCookie } from '@/utils/cookie';
 
-// Reroutes unauthenticated users
-export async function middleware(request: NextRequest) {
-  const result = await getCookie('session');
-  if (result?.obj) {
-    return NextResponse.next();
-  } else {
+export function middleware(request: NextRequest) {
+  const session = request.cookies.get('session')?.value;
+
+  const isAuthenticated = !!session;
+  const isAtRoot = request.nextUrl.pathname === '/';
+
+  if (!isAuthenticated && !isAtRoot) {
     return NextResponse.redirect(new URL('/', request.url));
   }
+
+  if (isAuthenticated && isAtRoot) {
+    return NextResponse.redirect(new URL('/campaigns', request.url));
+  }
+
+  // Otherwise allow the request
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/campaigns/:path*'],
+  matcher: ['/', '/campaigns/:path*'],
 };
